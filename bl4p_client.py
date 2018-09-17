6fbae4ac658d68578ac8c9268922c2bc1155e254
Corné Plooy <corne@bitonic.nl>
2018-09-17 13:03:10 +0200

    Add simple console interface and start of the client class

---
+++ bl4p_client.py
@@ -0,0 +1,30 @@
+#    Copyright (C) 2018 by Bitonic B.V.
+#
+#    This file is part of BL4P client.
+#
+#    BL4P client is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    BL4P client is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with BL4P client. If not, see <http://www.gnu.org/licenses/>.
+
+from bl4p_api import client as bl4p
+
+
+
+class BL4PClient:
+	def __init__(self):
+		self.connection = bl4p.Bl4pApi('ws://localhost:8000/', '3', '3')
+		self.orders = []
+
+
+	def close(self):
+		self.connection.close()
+
---
+++ bl4p_client_console.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+#    Copyright (C) 2018 by Bitonic B.V.
+#
+#    This file is part of BL4P client.
+#
+#    BL4P client is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    BL4P client is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with BL4P client. If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+
+import bl4p_client
+
+
+
+client = bl4p_client.BL4PClient()
+
+
+
+def stop():
+	'Terminate application.'
+	client.close()
+	sys.exit()
+
+
+def help():
+	'Display this message.'
+	names = list(commands.keys())
+	names.sort()
+	maxLen = max(map(len, names))
+	for name in names:
+		printedName = name.ljust(maxLen, ' ')
+		description = commands[name].__doc__
+		print('%s: %s' % (printedName, description))
+
+
+def license():
+	'Display licensing information.'
+	print('''BL4P Client is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+BL4P Client is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BL4P Client. If not, see <http://www.gnu.org/licenses/>.''')
+
+
+commands = \
+{
+'stop': stop,
+'quit': stop,
+'help': help,
+'license': license
+}
+
+def handleCommand(cmd):
+	cmd = cmd.strip() # remove whitespace
+
+	if not cmd:
+		return
+
+	cmd = cmd.lower()
+
+	try:
+		cmd = commands[cmd]
+	except KeyError:
+		print('No such command: "%s". Enter "help" for a list of commands.' % cmd)
+		return
+
+	cmd()
+
+
+print('''
+BL4P Client Copyright (C) 2018
+Enter 'help' for a list of commands. Enter 'license' for licensing information.
+''')
+
+while True:
+	cmd = input('> ')
+	try:
+		handleCommand(cmd)
+	except Exception as e:
+		print(str(e))
+
